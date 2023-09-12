f997df71de1a45f1165f944ec1dcef232d5cb856
S-dev-coder <sk7137389@gmail.com>
2023-09-13 02:29:52 +0530

    Q8

--- Recursion/Q8.N_Queen.cpp
+++ Recursion/Q8.N_Queen.cpp
@@ -1,1 +1,100 @@
-hi
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+    bool isSafe1(int row, int col, vector < string > board, int n) {
+      // check upper element
+      int duprow = row;
+      int dupcol = col;
+
+// check in the upper diagonal
+      while (row >= 0 && col >= 0) {
+        if (board[row][col] == 'Q')
+          return false;
+        row--;
+        col--;
+      }
+
+// check in the same row by decreasing column
+      col = dupcol;
+      row = duprow;
+      while (col >= 0) {
+        if (board[row][col] == 'Q')
+          return false;
+        col--;
+      }
+// check in the lower diagonal
+      row = duprow;
+      col = dupcol;
+      while (row < n && col >= 0) {
+        if (board[row][col] == 'Q')
+          return false;
+        row++;
+        col--;
+      }
+      return true;
+    }
+
+
+
+    void solve(int col, vector < string > & board, vector < vector < string >> & ans, int n) {
+      if (col == n) {
+        ans.push_back(board);
+        return;
+      }
+      for (int row = 0; row < n; row++) {
+
+        // will put Q only where it will be a safe position
+        if (isSafe1(row, col, board, n)) {
+            // if it is safe then place Q there
+          board[row][col] = 'Q';
+          // call recursive for the next position check .
+          solve(col + 1, board, ans, n);
+          // after doing all the possible recursive calls just remove that Q from the position and make it blank to generate other posibilities.
+          board[row][col] = '.';
+        }
+      }
+    }
+
+
+
+
+// Input: n = 4
+
+// Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+//         ||string|                                                   |
+//         ||_________________________board___________________________||
+//         |______________________________ans__________________________| 
+
+
+
+
+
+    vector < vector < string >> solveNQueens(int n) {
+      vector < vector < string >> ans;
+      vector < string > board(n);
+      string s(n, '.');
+      for (int i = 0; i < n; i++) {
+        board[i] = s;
+      }
+      solve(0, board, ans, n);
+      return ans;
+    }
+
+
+int main() {
+  int n = 4; // we are taking 4*4 grid and 4 queens
+  Solution obj;
+  vector < vector < string >> ans =solveNQueens(n);
+  for (int i = 0; i < ans.size(); i++) {
+    cout << "Arrangement " << i + 1 << "\n";
+    for (int j = 0; j < ans[0].size(); j++) {
+      cout << ans[i][j];
+      cout << endl;
+    }
+    cout << endl;
+  }
+  return 0;
+}
+
