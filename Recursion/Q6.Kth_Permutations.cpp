873e00ee2185f8c947abd29e89544d67aef0adb8
S-dev-coder <sk7137389@gmail.com>
2023-09-13 02:16:02 +0530

    Q6

--- Recursion/Q6.Kth_Permutations.cpp
+++ Recursion/Q6.Kth_Permutations.cpp
@@ -1,1 +1,59 @@
-hi
+
+// Example 1:
+
+// Input: N = 3, K = 3
+ 
+// Output: “213”
+
+// Explanation: The sequence has 3! permutations as illustrated in the figure above. K = 3 corresponds to the third sequence.
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+    //function to generate all possible permutations of a string
+    void permutationHelper(string & s, int index, vector < string > & res) {
+      if (index == s.size()) {
+        res.push_back(s);
+        return;
+      }
+      for (int i = index; i < s.size(); i++) {
+        swap(s[i], s[index]);
+        permutationHelper(s, index + 1, res);
+        swap(s[i], s[index]);
+      }
+    }
+
+
+  string getPermutation(int n, int k) {
+    string s;
+    vector < string > res;
+    //create string
+    for (int i = 1; i <= n; i++) {
+      s.push_back(i + '0');
+    }
+    permutationHelper(s, 0, res);
+    //sort the generated permutations
+    sort(res.begin(), res.end());
+    //make k 0-based indexed to point to kth sequence
+    // k-1 th index permutation will be the kth permutation.
+    auto it = res.begin() + (k - 1);
+    return *it;
+  }
+
+
+
+
+int main() {
+  int n = 3, k = 3;
+  Solution obj;
+  string ans =  getPermutation(n, k);
+  cout << "The Kth permutation sequence is " << ans << endl;
+  return 0;
+}
+
+
+
