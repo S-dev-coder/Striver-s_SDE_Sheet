d3fd7085e2176b762f941a5102c51ed3463db667
Sakshi Kumari <80236932+S-dev-coder@users.noreply.github.com>
2023-09-19 13:03:44 +0530

    Q18. SOlution Code

---
+++ Arrays/Q18.2Sum.cpp
@@ -0,0 +1,56 @@
+// Brute force approach -- Time Complexity: O(N2)  Space Complexity: O(1)
+
+
+vector<int> twoSum(int n, vector<int> &arr, int target) {
+    vector<int> ans;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] == target) {
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
+            }
+        }
+    }
+    return { -1, -1};
+}
+
+
+
+
+// Better Approach-- Time Complexity: O(N)  Space Complexity: O(N)
+
+
+vector<int> twoSum(int n, vector<int> &arr, int target) {
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < n; i++) {
+        int num = arr[i];
+        int moreNeeded = target - num;
+        if (mpp.find(moreNeeded) != mpp.end()) {
+            return {mpp[moreNeeded], i};
+        }
+        mpp[num] = i;
+    }
+    return { -1, -1};
+}
+
+
+
+// optimal approach - Time Complexity: O(N) + O(N*logN)  Space Complexity: O(1)
+
+string twoSum(int n, vector<int> &arr, int target) {
+    sort(arr.begin(), arr.end());
+    int left = 0, right = n - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == target) {
+            return "YES";
+        }
+        else if (sum < target) left++;
+        else right--;
+    }
+    return "NO";
+}
+
+
+
